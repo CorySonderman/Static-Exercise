0a87660f71165d3b1f4215f0bccc7be520405c4f
CorySonderman <sonderman2000@yahoo.com>
2023-10-08 15:09:10 -0500

    Completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace StaticExercise
 {
     public class Program
-    {
+    {    
+        //Now create a static class called TempConverter. The class should have at
+        //least 2 methods, one called FahrenheitToCelsius that will require a double
+        //as a parameter and return a double, the other CelsiusToFahrenheit which will
+        //also require a double as a parameter and return a double.
+        //Fill out these methods and call them in your Program.cs file.Make sure
+        //they’re accurate!
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine("--------------Conversion Results---------------");
+            Console.WriteLine($"celcius: {celcius}");
+            Console.WriteLine();
+            Console.WriteLine($"fahrenheit: {fahrenheit}");
         }
+
+
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //Now create a static class called TempConverter. The class should have at
+        //least 2 methods, one called FahrenheitToCelsius that will require a double
+        //as a parameter and return a double, the other CelsiusToFahrenheit which will
+        //also require a double as a parameter and return a double.
+        public static double FahrenheitToCelsius(double farhenheit) 
+        {
+           var result = (farhenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+
+    }
+}
